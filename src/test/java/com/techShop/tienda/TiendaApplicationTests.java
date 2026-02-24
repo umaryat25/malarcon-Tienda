c244928e65993f176f4fdf6b7e0504c767964e27
May-prog <umaryalar@gmail.com>
2026-02-24 10:59:02 -0600

    PrimeraClaseTienda

---
+++ src/test/java/com/techShop/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techShop.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
